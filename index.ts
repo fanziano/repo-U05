08cf1d6c786247bf89b2ca16d079869aadfba8d7
FerAnz <fanziano@yahoo.com.ar>
2021-10-07 17:42:10 -0300

    First commit

---
+++ index.ts
@@ -0,0 +1,33 @@
+class Album {
+    title:string
+    songs:string[]
+    constructor(theTitle:string, theSongs:string[]){  
+        this.title = theTitle;
+        this.songs = theSongs;
+    }
+}
+
+class Banda {
+    members:string[]
+    albums:Album[]
+    constructor(theMembers:string[], theAlbums:Album[]) {
+        this.members = theMembers;
+        this.albums = theAlbums;
+    }
+}
+
+////
+
+function main() {
+
+    const Album1 = {
+        title: "Sui en vivo",
+        songs: ["The Canterville Ghost", "Quizá porque", "Fabricante de Mentiras"],
+    };
+    
+    const Banda1 = new Banda(["Charly", "Nito"], [Album1]);
+
+    console.log(Banda1.albums);
+}
+
+main();
